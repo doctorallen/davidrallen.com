c9128ce43081d289d0ce7fba40d1127bec3a2905
drallen1 <drallen1@plymouth.edu>
2012-06-20 20:54:37 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php include('includes/header.html'); ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<div id="container">
+		<div id="center" class="column">
+
+     <span class="name1">DAVID</span> <span class="name2"> ALLEN</span>                              
+		<h2>Contact</h2>    
+		
+			<ul id="contact">
+				<li><span class="head">Cell:</span>(603)236-6988</li>
+                <li><span class="head">E-mail:</span>drallen1@plymouth.edu</li>
+                <li><span class="head">Address:</span> 26 Carpenter Park Road, Plymouth, New Hampshire 03264</li>   
+            </ul>        
+			
+         <h2>Objective</h2>   
+		 
+           <p>
+           To obtain a position that will further my career and my understanding of the Information Technology world.
+          </p>
+                                            
+         <h2>Key Skills</h2>
+		 
+           <p>
+            Proficient or familiar with an extensive array of programming languages, concepts and programs, including:
+           </p>
+			<div class="skill">
+			<span class="head">System Administration:</span> Active Directory, Linux/Unix Administration, Networking fundamentals, 
+			Virtual Desktop Infrastructure, VMware Veeam, Citrix XenDesktop, PERL, Windows Server 2008;
+			</div>
+			<div class="skill">
+			<span class="head">Communication/Leadership:</span> Public Speaking, Leadership Seminars, Group Leader, 
+			</div>
+			<div class="skill">
+			<span class="head">Web Development:</span> PHP, MYSQL, Smarty, MVC Frameworks, RESTful APIs,  XHTML, jQuery, CSS, Javascript, VIM;
+			</div>
+			<div class="skill">
+			<span class="head">Application Design:</span> C#, C++, XNA, Java, VB.NET, Visual Basic;
+			</div>
+			<div class="skill">
+			<span class="head">Other proficient software:</span> Photoshop, Excel, Access, Powerpoint, Flash, Windows (XP,Vista,7), MAC OS X
+			</div>
+		
+
+                                                
+            <h2>Education</h2>
+                                   
+            <div class="school">                                   
+				<span class="head">Bachelor of Science in Information Technology - 2012 </span>
+				<span class="right">2010 to 2012</span><br>                                        
+				<span class="add">Plymouth State University, Plymouth, NH - Magna Cum Laude - GPA 3.52</span><br>
+				<span class="add">Top 20 Senior Award, Outstanding Student in Information Technology Award</span>
+             </div>
+						                                                
+             <h2>Work Experience</h2>
+			 
+			<div class="job">
+              		<span class="head">Junior Developer</span>      
+				<span class="right">Full-time Job</span><br>			  
+              		<span class="add">Plymouth State University, Plymouth, NH</span>                                     
+			    <span class="right">May 2012 to Current</span>
+                 <ul>
+					 <li>Designed and developed an open source full-scale equipment reservation web application system;</li>
+					 <li>Used MVC Frameworks, code was peer reviewed, system designed for 5000+ users;</li>
+				 </ul>
+               </div> 
+
+			<div class="job">
+              		<span class="head">Computer Science Undergraduate Fellow</span>      
+				<span class="right">Internship</span><br>			  
+              		<span class="add">Plymouth State University, Plymouth, NH</span>                                     
+			    <span class="right">May 2011 to Current</span>
+                 <ul>
+					 <li>Worked closely with professors and students with creating and offering support for lab assignments;</li>
+					 <li>Maintained and updated networking equipment and computers for computer science labs;</li>
+				 </ul>
+               </div> 
+			   
+			   <div class="job">
+				<span class="head">Information Desk Consultant (Photoshop, Flash)</span>
+				<span class="right">Part-time job</span><br>
+				<span class="add">Plymouth State University, Plymouth, NH</span>
+				<span class="right">September 2010 to Current</span>
+                <ul>
+					<li>Media Presentation Center (Photoshop, Flash, Apple Software);</li>
+					<li>Call Center Support for over 400 faculty and staff and over 4000 students;</li>
+				</ul>
+				</div>
+				
+				<div class="job">
+				<span class="head">Systems Support Intern (VDI, Xen Desktop, Veeam)</span>
+				<span class="right">Internship</span><br>
+				<span class="add">Plymouth State University, Plymouth, NH</span>
+				<span class="right">February 2011 to May 2011</span>
+				<ul>
+					<li>Worked as lead in researching and testing virtual desktop infrastructure services;</li>
+					<li>Wrote up technical documentation on how to install and manage tested services;</li>
+				</ul>
+				</div>
+				
+                <div class="job">
+				<span class="head">TechBench (Computer Repair, Virus Removal) </span>
+				<span class="right">Part-time job</span><br>
+				<span class="add">Daniel Webster College ITS, Nashua, NH</span>
+				<span class="right">November 2008 to  May 2010</span>                                  
+                <ul>
+					<li>Troubleshot numerous hardware and software related computer issues; Virus Removal;</li>
+					<li>Worked as a team to develop a 100 page technical document;</li>
+				</ul>
+				</div>
+				
+				
+				<div class="job">
+				<span class="head">LabTech (Active Directory, Norton Ghost) </span>
+				<span class="right">Part-time job</span><br>
+				<span class="add">Daniel Webster College ITS, Nashua, NH</span>
+				<span class="right">September 2008 to  May 2010</span>                                  
+                <ul>
+					<li>Worked as a helpdesk associate troubleshooting network, printer and workstation issues;</li>
+					<li>Setup over 100 computers with new software, using Norton Ghost and other various tools.</li>
+				</ul>
+				</div>
+
+                <h2>Clubs/Organizations</h2>
+                 <ul>							  
+					  <li>President of the Gaming Club (Daniel Webster College) </li>		 
+					  <li>Vice President of Marketing for PACE (Plymouth State Univeristy) </li>					   
+			  </ul>
+
+			 <h2>Web Profile</h2>
+				<ul>
+					<li><a href="http://stackoverflow.com/users/984383/david-allen">Stack Overflow</a></li>
+					<li><a href="https://github.com/drallen1">Github</a></li>
+					<li><a href="http://davidrallen.com">Personal Website</a></li>
+					<li><a href="http://coderwall.com/drallen1">Coderwall</a></li>
+				</ul>
+                                                   
+
+	<div id="footer-wrapper">
+		<div id="footer">Website designed by David Allen. Copyright 2012.</div>
+	</div>
+		</div>
+	</div>
+	</body>
+</html>
