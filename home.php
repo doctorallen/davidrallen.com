1f9b6e961596e6ae38c66b0a11d282887346dd03
drallen1 <drallen1@plymouth.edu>
2012-07-10 18:15:33 +0000

    added bootstrap and homepage

---
+++ home.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+	<base target="_blank">
+	<script src="js/jquery-1.7.2.min.js"></script>
+	<script src="js/jquery.smooth-scroll.min.js"></script>
+	<script src="js/jquery-ui-1.8.18.custom.min.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<script src="pikachoose/lib/jquery.jcarousel.min.js"></script>
+	<script src="pikachoose/lib/jquery.pikachoose.full.js"></script>
+	<script src="pikachoose/lib/jquery.pikachoose.js"></script>
+	<script src="js/gallery.js"></script>
+	<script src="js/lightbox.js"></script>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-33243251-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>	
+	<link rel="stylesheet" type="text/css" href="../css/common.css" />
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap-responsive.css" />
+	<style type="text/css">
+		#main{
+			padding-top: 20px;
+		}
+	</style>
+<title>David Allen's Home Page</title>
+</head>
+<body>			
+	<div id="main">
+		<div class="navbar">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="brand" href="http://davidrallen.com/home">David's Home Page</a>
+					<ul class="nav">
+							<form class="navbar-search pull-right" method="get" action="http://www.google.com/search">
+
+								<input type="text" class="search-query span2" name="q" size="31" maxlength="255" value="" placeholder="Search Google" />
+							</form>
+						<b class="caret"></b>
+						
+					</ul>
+				</div>	
+			</div>
+		</div>
+		<div class="row">
+			<div class="span3">
+				<div class="well">
+					<ul class="nav nav-list">
+						<li class="nav-header">
+							Personal Sites
+						</li>
+						<li>
+							<a href="http://davidrallen.com"><i class="icon-home"></i>David Allen</a>
+						</li>
+						<li>
+							<a href="http://parahoax.com"><i class="icon-camera"></i>ParaHoax</a>
+						</li>
+						<li class="nav-header">
+							Social Media
+						<li>
+							<a href="http://facebook.com"><i class="icon-thumbs-up"></i>Facebook</a>
+						</li>
+						<li>
+							<a href="http://plus.google.com"><i class="icon-plus"></i>Google Plus</a>
+						</li>
+						<li>
+							<a href="http://twitter.com"><i class="icon-user"></i>Twitter</a>
+						</li>
+						<li class="nav-header">
+							Work Related
+						<li>
+							<a href="http://drallen1.psudev.com/webapp"><i class="icon-ok"></i>PSU Dev</a>
+						</li>
+						<li>
+							<a href="http://www.plymouth.edu/webapp/mis/wiki/Main_page"><i class="icon-book"></i>MIS Wiki</a>
+						</li>
+						<li>
+							<a href="http://www.plymouth.edu/webapp/time"><i class="icon-comment"></i>MIS Time</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="span3">
+				<div class="well">
+				asdf
+				</div>
+			</div>
+		</div>
+
+	<a class="btn btn-primary" href="http://moqups.com">Moqups</a>
+	</div>
+
+<div id="footer-wrapper">
+		<span class="footer">Website designed by David Allen. Copyright 2012.</span>
+	</div>
+	</body>
+</html>
