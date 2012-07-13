e915d25f708d8e615685347f9ee023ed2c738d20
drallen1 <drallen1@plymouth.edu>
2012-07-13 20:40:23 +0000

    added progress bar and development page

---
+++ development.php
@@ -0,0 +1,33 @@
+<?php include('includes/header.html'); ?>
+<div id="development">
+	<h2>Current Development Projects</h2>
+	<div class="port">
+		<h3>Resume Builder</h3>
+		<p class="desc">
+			A web application for creating, editing, saving, displaying, sharing, and styling resumes.
+		</p>
+		<h4>Progress</h4>
+		<span class="badge badge-info">20%</span>
+		<div class="progress progress-striped active" style="width: 300px; margin-bottom: 9px;">
+			<div class="bar" style="width: 20%;"></div>
+		</div>
+		<h4>Technologies Used:</h4>
+		<ul class="tech">
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>Code Igniter</li>
+		</ul>
+	</div>
+	<!--
+	<div class="port">
+		<h3></h3>
+		<p class="desc"></p>
+		<h4>Technologies Used:</h4>
+		<ul class="tech">
+			<li></li>
+		</ul>
+	</div>
+
+	-->
+</div>
+<?php include('includes/footer.html'); ?>
