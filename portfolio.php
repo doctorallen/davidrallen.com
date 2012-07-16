2f71bc6beff944ff029cdc44596dc7da4e469b77
drallen1 <drallen1@plymouth.edu>
2012-07-16 14:40:11 +0000

    added cts image

--- portfolio.php
+++ portfolio.php
@@ -32,7 +32,7 @@
 	</ul>
 	</div>
 	<div class="port">
-		<h3 id="CTS">Classroom Technology Services Toolkit</h3>
+		<h3 id="CTS"><a href="images/cts_full.png" rel="lightbox" title="CTS Toolkit">Classroom Technology Services Toolkit</a></h3>
 		<p class="desc">The Plymouth State Classroom Technology Services Toolkit was a full-scale web application that was built for the use of all students, faculty and staff and Plymouth State University. It's functionalities allow authenticated users to make equipment requests, and has a very large selection of administrative tools that allows the manager of the Classroom Technology Servcices Department (CTS) to assign his staff members to reservations for support, but also allows his staff to view available equipment from the Unversities inventory control system GLPI, assign equipment and edit loans, view their assignments, as well as support for help desk personal who will be the face to face contact for the person who created the loan. I took the project from product specification to production in only 5 months, after a rigourous code-review and debug process.   </p>
 		<h4>Technologies Used:</h4>
 		<ul class="tech">
