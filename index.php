e84590c3f1833125ff056aea34d35e3c90d3298c
David Allen <drallen1@plymouth.edu>
2013-03-27 19:42:33 +0000

    added klein routing

--- index.php
+++ index.php
@@ -1,135 +1,23 @@
-<?php include('includes/header.html'); ?>
-	<div id="left">
-				<h2>Contact</h2>
-				  <ul class="contact">
-					 <li><span>trooper898@gmail.com</span></li>
-					 <li><span>(603) 236-6988</span></li>
-					 <li><span><a href="https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=plymouth,+nh&aq=&sll=37.0625,-95.677068&sspn=54.928982,79.013672&vpsrc=0&t=h&ie=UTF8&hq=&hnear=Plymouth,+Grafton,+New+Hampshire&z=12" target="_blank" >Plymouth, NH 03264</a><span></li>   
-				  </ul>        
-					
-												   
-			    <h2>Proficiencies</h2>
-				 
-					<div class="skill">
-					<span class="head">Languages</span><p>PHP, HTML5, CSS3, JavaScript, jQuery, Java, VB .NET, C#, XNA</p>
-					</div>
-					<div class="skill">
-					<span class="head">Databases</span><p>MySQL, Access</p>
-					</div>
-					<div class="skill">
-					<span class="head">Frameworks</span><p>Klein Routing, Smarty, Codeigniter, CakePHP, Wordpress, Node.js, Socket.io, Express.js</p>
-					</div>
-					<div class="skill">
-					<span class="head">Software</span><p>Git, Subversion, VIM, Adobe Photoshop</p>
-					</div>
-					<div class="skill">
-					<span class="head">Operating Systems</span><p>Windows, Mac OSX, Linux, VMware vSphere, XenServer</p>
-					</div>
-					<div class="skill">
-					<span class="head">Web Administration</span><p>LAMP Server(Linux, Apache, MySQL, PHP), Amazon AWS(EC2, VPC, ELB, EC2, Cloudfront, SES, Route 53), NGINX, NodeJS</p>
-					</div>
+<?php
+require 'klein.php';
+respond('/', function( $request, $response, $app ){
+  $response->render('home.php');
+});
 
-				<h2>Web Portfolio</h2>
-						<ul class="profile">
-							<li><a href="http://stackoverflow.com/users/984383" target="blank">Stack Overflow</a></li>
-							<li><a href="https://github.com/drallen1" target="blank">Github</a></li>
-							<li><a href="http://coderwall.com/drallen1" target="blank">Coderwall</a></li>
-						</ul>
-			
+respond('/home', function( $request, $response, $app ){
+  $response->render('home.php');
+});
 
-											   
-				  <h2>Education</h2>
-									
-				  <div class="school right-aligned">                                   
-						<span class="school-name">Plymouth State University</span><br>
-						<span class="normal">B.S. Information Technology</span><br>
-						<span class="award">Magna Cum Laude,<br> Top 20 Senior Award,<br> Outstanding Student in <br> Information Technology Award</span>
-				   </div>
-	</div>
-	<div id="right">
-			<h1>Open Source Web Developer</h1>
+respond('/portfolio', function( $request, $response, $app ){
+  $response->render('portfolio.php');
+});
 
-			<h2>Objective</h2>   
-			 
-			 <p>
-				To create valuable and exciting web applications as an open source web developer, learn new technologies and languages, stay on the leading edge and make a positive impact on the web and its users.
-			</p>
+respond('/about', function( $request, $response, $app ){
+  $response->render('about.php');
+});
 
-			   <h2>Experience</h2>
-				 
-				<div class="job">
-					<span class="head">The Atom Group</span>
-					<span class="right-head">August 2012 - Present</span>
-					<br>
-					<span class="job-title">Web Developer</span>      
-				    <span class="right-location">Porstmouth, NH</span>
-				  <ul>
-					<li>Created Web Applications using Codeigniter, CakePHP, Wordpress, Drupal, Node.js, Express.js, Socket.io </li>
-				  </ul>
-				</div>
-				<div class="job">
-					<span class="head">Plymouth State University</span>
-					<span class="right-head">May 2012 - August 2012</span>
-					<br>
-					<span class="job-title">Junior Developer</span>      
-				    <span class="right-location">Plymouth, NH</span>
-				  <ul>
-						 <li>Designed and Developed a <a href="portfolio.php#CTS">full-scale open source web application</a> using PHP, MySQL, HTML5, CSS3, jQuery, JavaScript, Smarty and Klein routing in a LAMP environment using VIM and Git</li>
-						 <li>Used MVC Framework with Smarty/HTML5 and PHP/MySQL/Klein</li>
-						 <li>Developed Project from specification to production in 5 months</li>
-						<li>Integrated with existing MySQL database, Plymouth State API Layer, and GLPI</li>
-						<li>Created technical documentation and end-user manuals</li>
-						<li>All code was peer reviewed and open-sourced on github.com</li>
-						<li><a href="portfolio#github">Migrated</a> existing web applications from Subversion to GitHub</li>
-					 </ul>
-				</div>
+respond('/development', function( $request, $response, $app ){
+  $response->render('development.php');
+});
 
-				<div class="job">
-					<span class="head">Plymouth State University</span>
-					<span class="right-head">Sept 2011 - May 2012</span>
-					<br>
-					<span class="job-title">Computer Science Undergraduate Fellow</span>      
-				    <span class="right-location">Plymouth, NH</span>
-				  <ul>
-						 <li>Worked alongside professors in creating and offering support for lab assignments </li>
-						 <li>Maintained and updated networking equipment and computers for computer science labs</li>
-				 </ul>
-				</div> 
-				
-				<div class="job">
-					<span class="head">Plymouth State University</span>
-					<span class="right-head">Feb 2011 - May 2011</span>
-					<br>
-					<span class="job-title">Systems Support Intern</span>      
-				    <span class="right-location">Plymouth, NH</span>
-				  <ul>
-						 <li>Worked as lead in researching and testing <a href="portfolio#support">virtual desktop infrastructure</a> services</li>
-						 <li>Created technical documentation for installing and managing tested services</li>
-				 </ul>
-				</div> 
-
-				<div class="job">
-					<span class="head">Plymouth State University</span>
-					<span class="right-head">Sept 2010 - May 2012</span>
-					<br>	
-					<span class="job-title">Information Desk Consultant</span>      
-				    <span class="right-location">Plymouth, NH</span>
-				  <ul>
-						 <li>Call Center supporting 60,000+ university constituents, 1200+ calls logged</li>
-						 <li>Supported Media Presentation Center (Photoshop, Flash, iMovie)</li>
-				 </ul>
-				</div> 
-				
-				<div class="job">
-					<span class="head">Daniel Webster College</span>
-					<span class="right-head">Nov 2008 - May 2010</span>
-					<br>
-					<span class="job-title">TechBench</span>      
-				    <span class="right-location">Nashua, NH</span>
-				  	<ul>
-						 <li>Troubleshot numerous hardware and software related computer issues</li>
-						 <li>Collaborated with a team on the creation of a 100 page technical manual for helpdesk operation</li>
-				 	</ul>
-				</div> 
-	</div>
-<?php include('includes/footer.html'); ?>
+dispatch();
