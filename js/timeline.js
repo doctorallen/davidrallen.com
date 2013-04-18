335efd897b0cf92050e94e9090bc5ba43262bb84
David Allen <drallen1@plymouth.edu>
2013-04-18 17:32:59 +0000

    added real data to timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,4 +1,4 @@
-var months = {1: 'January', 2:'Feburary', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 11:'December'};
+var months = {1: 'January', 2:'Feburary', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'};
 
 var dates = [];
 
